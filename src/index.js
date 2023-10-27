c0c0fb05758118ba329001ea90a2ef90bfdef8a0
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2023-10-27 16:57:48 +0300

    3.0.0-27.10.23

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('#search-form');
-    const input = form.querySelector('input');
-    const button = form.querySelector('button');
-    const loadMore = document.querySelector('.load-more');
-    const gallery = document.querySelector('.gallery');
-});
 
+const form = document.querySelector('#search-form');
+const input = form.querySelector('input');
+const button = form.querySelector('button');
+const loadMore = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
+
+let item = '';
+let page = 1;
+const dataResult = {};
 
 let searchInput = '';
 input.addEventListener('input', () => {
@@ -17,25 +19,35 @@ input.addEventListener('input', () => {
 });
 
 button.addEventListener('click', async (event) => {
-    event.preventDefault()
+    event.preventDefault();
+    loadMore.classList.add('visually-hidden');
     try{
-        const searchResults = await search(40, 1);
+        page = 1;
+        await search(40, page);
+        const searchResults = dataResult.responses;
+        console.log(dataResult.numberOfResponses);
         list (searchResults);
+        gallery.innerHTML = item;
         libraryForGallery();
+        page = 2;
         if(!gallery.firstChild){
-            return false;
+            return error;
         }
+        loadMore.classList.remove('visually-hidden');
     }
-    catch{
+    catch(error){
         Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
     }
 });
 
 loadMore.addEventListener('click', async () => {
-    console.log(1);
     try{
-        const moreResults = await search(20, 2);
+        page += 1;
+        await search(20, page);
+        const moreResults = dataResult.responses;
+        console.log(dataResult.numberOfResponses);
         list(moreResults);
+        gallery.insertAdjacentHTML('beforeend', item);
         libraryForGallery();
     }
     catch{
@@ -46,11 +58,12 @@ loadMore.addEventListener('click', async () => {
 async function search(perPage, page){
     const  answer = await fetch(`https://pixabay.com/api/?key=40289268-709deefe1360f0520e7e421a0&q=${searchInput}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`);
     const result = await answer.json();
-    return result.hits;
+    dataResult.numberOfResponses = result.totalHits,
+    dataResult.responses = result.hits;
 };
 
 function list (searchResults){ 
-    const item = searchResults.map(
+    item = searchResults.map(
     (searchResult) => 
     `<div class="photo-card">
     <a href="${searchResult.largeImageURL}">
@@ -77,7 +90,7 @@ function list (searchResults){
     </div>`
 )
 .join("");
-gallery.innerHTML = item;
+return item;
 };
 
 function libraryForGallery(){
